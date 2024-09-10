c0b23a9ac131a70644b11f0ce901e2c9ef8501d6
Johnny-code77 <matoloanemolapo7@gmail.com>
2024-09-10 08:40:38 +0200

    code update

---
+++ app/cool-mode/page.tsx
@@ -0,0 +1,10 @@
+import { CoolMode } from '@/components/magicui/cool-mode'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <CoolMode children={undefined} />
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 // app/page.tsx
 import React from 'react';
 import Home from '../components/home'; // Adjust the path if needed
-import { BorderBeam } from '@/components/magicui/border-beam';
 
 const Page: React.FC = () => {
   return <Home />
   ;
 
-      <BorderBeam />
     
 };
 
